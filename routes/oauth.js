3f52e1e0adefbcf12a8a0598e475c7b343042323
consultingninja <consultingninja.tech@gmail.com>
2023-06-15 09:44:44 -0500

    re-work for gsi

--- routes/oauth.js
+++ routes/oauth.js
@@ -19,33 +19,14 @@ async function testOpen(id_token){
   console.log('Opend ID data',data);
 }
 
-//This function will get the current Discovery Doc from Google, then extract out the URI for obtaining current public keys to use if needing to implement your own auth for some unknown reason.
-async function getKeys(){
-  //first get Discovery Doc for jwks_uri 
-  const response = await fetch(`https://accounts.google.com/.well-known/openid-configuration`);
-  //console.log('response',response);
-  const data = await response.json();
-  //console.log('Discovery Doc',data);
-
-  //Use jwks_uri to get current keys
-  const jwtCerts = await fetch(data.jwks_uri);
-  const jwtData = await jwtCerts.json();
-  //console.log('Certs',jwtData);
-
-  return jwtData
-  
-}
-
-
-
 
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     const code = req.query.code;
 
-    console.log(code);
+    console.log('code',code);
     try {
         const redirectURL = "http://127.0.0.1:3000/oauth"
         const oAuth2Client = new OAuth2Client(
@@ -53,23 +34,26 @@ router.get('/', async function(req, res, next) {
             process.env.CLIENT_SECRET,
             redirectURL
           );
-        const r =  await oAuth2Client.getToken(code);
+
+        //const r =  await oAuth2Client.getToken(code);
         // Make sure to set the credentials on the OAuth2 client.
-        await oAuth2Client.setCredentials(r.tokens);
-        console.info('Tokens acquired.');
-        const user = oAuth2Client.credentials;
-        console.log('credentials',user);
+        //await oAuth2Client.setCredentials(r.tokens);
+        //console.info('Tokens acquired.');
+        //const user = oAuth2Client.credentials;
+        //console.log('credentials',user);
 
         //get info with access_token
         //await getUserData(user.access_token);
 
         //get info with id_token
         //await testOpen(user.id_token);
-        //const keys = await getKeys();
 
-        const ticket = await oAuth2Client.verifyIdToken({idToken:user.id_token,audience:process.env.CLIENT_ID,});
+        const ticket = await oAuth2Client.verifyIdToken({idToken:code,audience:process.env.CLIENT_ID,});
 
         console.log('ticket',ticket);
+
+        res.status(200);
+        res.json(JSON.stringify(ticket))
 
 
       } catch (err) {
@@ -77,7 +61,7 @@ router.get('/', async function(req, res, next) {
     }
 
 
-    res.redirect(303, 'http://localhost:5173/');
+
   
 
 });
